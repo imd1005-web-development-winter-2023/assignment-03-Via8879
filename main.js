0793305473f6002251d53a37ca9c7f2906541ac1
Via8879 <124936186+Via8879@users.noreply.github.com>
2023-03-26 14:05:00 -0400

    Update

--- main.js
+++ main.js
@@ -1,26 +1,103 @@
-//
-//  JS File
-//  You may remove the code below - it's just boilerplate
-//
+const form = document.querySelector(".form1");
+const todoTextFromForm = document.querySelector("#todo-item");
 
-//
-// Variables
-//
+const todoList = document.querySelector(".todolist1");
+// const todos = ["chips"];
 
-// Constants
-const appID = "app";
-const headingText = "To do. To done. ✅";
+const todos = [];
 
-// Variables
+function drawToDoList() {
+  // Clear all of the entries in the list
+  while (todoList.firstChild) {
+    todoList.removeChild(todoList.firstChild);
+  }
 
-// DOM Elements
-let appContainer = document.getElementById(appID);
+  for (let i = 0; i < todos.length; i++) {
+    const listItem = document.createElement("li");
+    listItem.textContent = todos[i].text;
 
-//
-// Functions
-//
+    if (todos[i].isDone === true) {
+      listItem.classList.add("done");
+    }
 
-// Add a heading to the app container
+    const todoDeleteButton = document.createElement("button");
+    todoDeleteButton.textContent = "X";
+    todoDeleteButton.classList.add("todoDeleteButton");
+
+    todoDeleteButton.dataset.index = i;
+
+    todoDeleteButton.addEventListener("click", deleteTodo);
+
+    const todoDoneButton = document.createElement("button");
+
+    if (todos[i].isDone === true) {
+      todoDoneButton.textContent = "Undo";
+      todoDoneButton.classList.add('undone-button');
+    } else {
+      todoDoneButton.textContent = "Done";
+      todoDoneButton.classList.add('done-button');
+    }
+    
+
+    todoDoneButton.dataset.index = i;
+
+    todoDoneButton.addEventListener("click", doneTodo);
+
+
+    listItem.appendChild(todoDoneButton);
+    listItem.appendChild(todoDeleteButton);
+
+    todoList.appendChild(listItem);
+  }
+}
+
+function doneTodo(event) {
+  console.log("Marked as done");
+
+  todoDeleteIndex = event.target.dataset.index;
+
+  console.log("INDEX: ", todoDeleteIndex);
+
+  todos[todoDeleteIndex].isDone = !todos[todoDeleteIndex].isDone;
+
+  console.log(todos);
+
+  drawToDoList();
+}
+
+function deleteTodo(event) {
+  console.log("Delete button index", event.target.dataset.index);
+
+  todoDeleteIndex = event.target.dataset.index;
+
+  todos.splice(todoDeleteIndex, 1);
+
+  drawToDoList();
+}
+
+function addTodo(event) {
+  event.preventDefault();
+
+  todoTextFromForm.value;
+
+  // todos.push(todoTextFromForm.value);
+
+  todos.push({
+    text: todoTextFromForm.value,
+    isDone: false,
+  });
+
+  console.log(todos);
+
+  form.reset();
+
+  drawToDoList();
+}
+
+form.addEventListener("submit", addTodo);
+
+//let appContainer = document.getElementById(appID);
+
 function inititialise() {
   // If anything is wrong with the app container then end
   if (!appContainer) {
@@ -36,9 +113,3 @@ function inititialise() {
   // Init complete
   console.log("App successfully initialised");
 }
-
-//
-// Inits & Event Listeners
-//
-
-inititialise();
